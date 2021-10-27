683743160895bc33d6c4efadc68a889c7b67d444
Endless <3130664193@qq.com>
2021-10-27 21:30:31 +0800

    commmit main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# Beautiful is better than ugly
+
+message = "Hello Python's "
+message1 = "world!"
+
+all_message = message + message1
+'''print("You can say "+all_message)
+
+print(message.title())
+print(message.upper())
+print(message.lower())'''
+print(message.lstrip() + message1)
+print('Albert Einstein once said, “A person who never made a '
+      'mistake never tried anything new.”')
+'''
+strip是删除的意思，我们通常用这一类方法对用户输入的内容进行规范
+'''
+universe_age=14_000_000_000
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
