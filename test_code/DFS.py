4f0094db31024f3b11fa51a6514e2645f7c03b83
Endless <3130664193@qq.com>
2022-01-26 11:29:25 +0800

    Changes

---
+++ test_code/DFS.py
@@ -0,0 +1,30 @@
+import time
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "C", "D"],
+    "C": ["A", "B", "D", "E"],
+    "D": ["B", "C", "E", "F"],
+    "E": ["C", "D"],
+    "F": ["D"]
+}
+
+
+def DFS(graph, s):
+    stack = [s]  # 创建一个stack
+    seen = {s}  # 创建一个存放已经加入过stack的set
+    while len(stack) > 0:  # 当stack不为空
+        """
+        pop 是唯一与BFS不同的地方
+        """
+        vertex = stack.pop()  # 最后一个元素出栈
+        nodes = graph[vertex]  # 取得与其相连的元素
+        for node in nodes:  # 遍历与其相连的元素
+            if node not in seen:  # 如果发现还未被加入stack的元素
+                stack.append(node)  # 将其加入stack
+                seen.add(node)  # 将其标记为已加入
+        print(vertex)
+
+
+print(time.time())
+DFS(graph, "F")
+print(time.time())
